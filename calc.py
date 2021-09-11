9d1040c72ebb9bd1dbcbc3952e42db530613e76d
Stpka <miholapstepan@gmail.com>
2021-09-11 22:36:23 +0300

    Add operations fact-al, sqrt

--- calc.py
+++ calc.py
@@ -1,17 +1,33 @@
 #  calculator 
 
+import math
+
 while True:
-	Operation = input("Select the operation (+, -, *, /): ")
-	FirstNumber = int(input("Input first number: "))
-	SecondNumber = int(input("Input second number: "))
-	if Operation == "+":
-		print(FirstNumber + SecondNumber)
+	ComplexityOfTheOperation = input("The complexity of the operation is basic/complex (B/C): ")
+	
+	if ComplexityOfTheOperation == 'B' or ComplexityOfTheOperation == 'b':
+		Operation = input("Select the operation (+, -, *, /): ")
+		FirstNumber = int(input("Input first number: "))
+		SecondNumber = int(input("Input second number: "))
 
-	elif Operation == "-":
-		print(FirstNumber - SecondNumber)
+		if Operation == "+":
+			print(FirstNumber + SecondNumber)
 
-	elif Operation == "*":
-		print(FirstNumber * SecondNumber)
+		elif Operation == "-":
+			print(FirstNumber - SecondNumber)
 
-	elif Operation == "/":
-		print(FirstNumber / SecondNumber)
+		elif Operation == "*":
+			print(FirstNumber * SecondNumber)
+
+		elif Operation == "/":
+			print(FirstNumber / SecondNumber)
+
+	elif ComplexityOfTheOperation == 'C' or ComplexityOfTheOperation == 'c':
+		Operation = input("Select the operation (!, sqrt): ")
+		Number  = int(input("Input number: "))
+
+		if Operation == '!':
+			print(math.factorial(Number))
+
+		elif Operation == 'sqrt':
+			print(math.sqrt(Number))
