8f7d1ccb23779cb2756fc37b25440545f5adc7a8
Stpka <miholapstepan@gmail.com>
2021-09-11 23:46:33 +0300

    Add discriminant

--- calc.py
+++ calc.py
@@ -1,10 +1,8 @@
 #  calculator 
 
 import math
-import rich
 from rich.console import Console
 from rich.table import Table
-
 
 
 console = Console() # I dont know what is it, but i must did it ;) 
@@ -19,30 +17,18 @@ table.add_row("-", "subtraction", "[red]I dont know... Realy[/red]")
 table.add_row("*", "multiplication", "The arithmetic action of repeating a given number by a term as many times as there are units in another given number, a factor.")
 table.add_row("/", "division", "An arithmetic operation, by which it is recognized how many times one number is contained in another.")
 table.add_row("!", "factorial", "Product of all natural numbers from 1 to a given natural number n")
-table.add_row("sin", "sinus", "")
-table.add_row("cos", "cosine", "")
+table.add_row("sin", "sinus", "the ratio of the leg of the opposite angle to the hypotenuse")
+table.add_row("cos", "cosine", "the ratio of the adjacent (close) leg to the hypotenuse")
 table.add_row("tan", "tangent", "The ratio of the opposite (distant) leg to the adjacent (close) leg.")
+table.add_row("D", "discriminant", "Her ego znaet :pile_of_poo: [RUS]")
+
 
 
 while True:
-	ComplexityOfTheOperation = input("The complexity of the operation is basic/complex (B/C) or help (h/help): ")
+	ComplexityOfTheOperation = input("The complexity of the operation is basic/complex/ultra (B/C/U) or help (h/help): ")
 	
 	if ComplexityOfTheOperation == 'h' or ComplexityOfTheOperation == 'help':
-
-
-
-
 		console.print(table)
-	
-		#print("Operation:  +  - addition")
-		#print("Operation:  -  - subtraction")
-		#print("Operation:  *  - multiplication")
-		#print("Operation:  /  - division")
-		#print("Operation:  !  - factorial")
-		#print("Operation: sin - sinus")
-		#print("Operation: cos - cosine")
-		#print("Operation: tan - tangent")
-		#print("- ======================= -")
 
 
 	elif ComplexityOfTheOperation == 'B' or ComplexityOfTheOperation == 'b':
@@ -81,3 +67,12 @@ while True:
 
 		elif Operation == 'tan':
 			print(math.tan(Number), "rad")
+
+
+	elif ComplexityOfTheOperation == 'U' or ComplexityOfTheOperation == 'u':
+		Operation = input("Select the operation (D): ")
+		if Operation == "D":
+			AForDiscriminant = int(input("Input A for discriminant: "))
+			BForDiscriminant = int(input("Input B for discriminant: "))
+			CForDiscriminant = int(input("Input C for discriminant: "))
+			print(BForDiscriminant**2 - 4 * AForDiscriminant * CForDiscriminant)
