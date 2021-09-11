1440c80f2c8d1a041ce8825b0f2e57f7e598b4d4
Stpka <miholapstepan@gmail.com>
2021-09-11 22:09:34 +0300

    Add operation +

--- calc.py
+++ calc.py
@@ -1,1 +1,7 @@
 #  calculator 
+
+Operation = input("Select the operation (+, -, *, /): ")
+FirstNumber = int(input("Input first number: "))
+SecondNumber = int(input("Input second number: "))
+if Operation == "+":
+	print(FirstNumber + SecondNumber)
